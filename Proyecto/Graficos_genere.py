391eb62abcde60a9db4b664c00d2727ca1523ef3
Nicolas <nicobenpe@gmail.com>
2025-06-14 18:28:28 -0300

    Analizando famale_winners

--- Proyecto/Graficos_genere.py
+++ Proyecto/Graficos_genere.py
@@ -4,33 +4,27 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 
 #Ruta del archivo
-
 nobel_df = pd.read_csv("C:/Users/Usuario/Desktop/Nico2/Proyectos/Premio_Nobel/data/nobel.csv")
 
-#Que porcetaje de ganadores tiene EEUU por decada?
-
-#Creando columna lugar de nacimiento EEUU
-nobel_df['EEUU_born_winner'] = nobel_df['birth_country'] == 'United States of America' 
+nobel_df['female_winner'] = nobel_df['sex'] == 'Female' 
 
 # Decada
 nobel_df ['decade'] = (np.floor(nobel_df['year']/10)*10).astype(int)
 
-#Promedio de cada ganador por decada
-prop_usa_winners = nobel_df.groupby('decade', as_index = False)['EEUU_born_winner'].mean()
+#Agrupación de mujeres ganadas
+prop_famele_winner = nobel_df.groupby(['decade', 'category'], as_index = False)['female_winner'].mean()
 
-
-
-#Graficas de lineas de los datos
 
 plt.rcParams['figure.figsize'] = [11,7]
 
 ax = sns.lineplot(
     x = 'decade',
-    y= 'EEUU_born_winner',
-    data= prop_usa_winners
+    y= 'female_winner',
+    data= prop_famele_winner,
+    hue= 'category'
 )
 
-plt.title('Proporción de ganadores nacidos en EEUU por década')
+plt.title('Proporción de ganadores de mujeres por década')
 plt.xlabel('Década')
 plt.ylabel('Proporción')
 plt.grid(True)
