3d953473f9df27e569b5b71b3c5eed2a6e1c3a0d
Nicolas <nicobenpe@gmail.com>
2025-06-14 16:06:22 -0300

    Más ganadores

--- Proyecto/Premio_nobel.py
+++ Proyecto/Premio_nobel.py
@@ -6,23 +6,43 @@ import seaborn as sns
 #Ruta del archivo
 
 nobel_df = pd.read_csv("C:/Users/Usuario/Desktop/Nico2/Proyectos/Premio_Nobel/data/nobel.csv")
-
+"""
 #Vizualizamos los 5 primeros datos
 #print(nobel_df.head())
+"""
 
+"""
 #Vizualizamos todos los datos del DF
 #print(nobel_df.info)
+"""
 
+"""
 #Que vamos analizar?
 #Como conocer el conjunto de forma general
-# - Cuantos premios tenemos regitstrados?
-# - Cuantos premios han ganado hombrey mujeres?
-# - Que paises han ganado más premios?
-# - Que categoria tiene más premios?
+# - Cuantos premios tenemos regitstrados? Existen 911 premios
+# - Cuantos premios han ganado hombrey mujeres? Los hombre tienen 836 premios y muejeres 49
+# - Que paises han ganado más premios? - Estados unidos tiene más premios ganados 
+# - Que categoria tiene más premios? La categoria medicina tiene más premios ganados 211
+"""
+
 
 """
 #Cantidad de Filas
 print(len(nobel_df))
 """
 
+
+"""
+#Cantidad de generos
 print(nobel_df['sex'].value_counts())
+"""
+
+"""
+#Vizualiza los 10 paises con más premios nobels
+print(nobel_df['birth_country'].value_counts().head(10))
+"""
+
+"""
+#Vizualiza las categorias con más premios nobels
+print(nobel_df['category'].value_counts())
+"""
